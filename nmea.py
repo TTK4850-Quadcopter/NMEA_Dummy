55ef57a1f770aa8e49968b469c7280343b67f2cb
Håkon Leithe <hakon@norbit.no>
2015-02-11 13:28:17 +0100

    I believe this could work

---
+++ nmea.py
@@ -0,0 +1,20 @@
+import socket
+import serial
+import time
+
+serialdevice = "/dev/ttyUSB0"
+
+port =serial.Serial(port=serialdevice, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=5)
+
+longitude = 0
+latitude = 0
+
+gga='$GPGGA,123519,4807.%03d,N,01131.%03d,E,1,08,0.9,545.4,M,46.9,M,,*47'% (latitude, longitude)
+
+while 1
+    gga='$GPGGA,123519,4807.%03d,N,01131.%03d,E,1,08,0.9,545.4,M,46.9,M,,*47'% (latitude, longitude)
+    port.write(gga + '\r\n')
+    latitude+=1
+    longitude+=1
+    time.sleep(1)
+
