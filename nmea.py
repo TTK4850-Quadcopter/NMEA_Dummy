afd8f6f6745904528195a1820a17a3f44a2a7556
Håkon Leithe <hakon@norbit.no>
2015-02-25 01:09:47 +0100

    Syntax error, and printing output

--- nmea.py
+++ nmea.py
@@ -11,8 +11,9 @@ latitude = 0
 
 gga='$GPGGA,123519,4807.%03d,N,01131.%03d,E,1,08,0.9,545.4,M,46.9,M,,*47'% (latitude, longitude)
 
-while 1
+while 1:
     gga='$GPGGA,123519,4807.%03d,N,01131.%03d,E,1,08,0.9,545.4,M,46.9,M,,*47'% (latitude, longitude)
+    print gga
     port.write(gga + '\r\n')
     latitude+=1
     longitude+=1
